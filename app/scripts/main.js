8f60de47428cf0239aef12e23b9af25d112599e3
Christian Acuna <cacuna0828@gmail.com>
2016-10-10 18:44:42 +0800

    add stored data

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -32,7 +32,8 @@ function getHarvardData() {
     type: 'GET',
     data: {
       'place': location,
-      'apikey': '0c781bd0-8a9f-11e6-bcde-977dd71a47a9'
+      'apikey': '0c781bd0-8a9f-11e6-bcde-977dd71a47a9',
+      'size': '50'
       // "keyword": "photographs"
     }
   }).done(function(data, textStatus, jqXHR) {
@@ -49,8 +50,48 @@ function getHarvardData() {
     addLightbox();
   }).fail(function(jqXHR, textStatus, errorThrown) {
     console.log('HTTP Request Failed');
+    getStoredHarvardData();
   }).always(function() {
     /* ... */
+  });
+}
+
+function getStoredHarvardData() {
+  var location = '';
+  var cityValue = document.getElementById('js-city').value;
+  switch (cityValue) {
+    case '香港':
+      location = '2028558';
+      break;
+    case '北京':
+      location = '2035839';
+      break;
+    case '上海':
+      location = '2040128';
+      break;
+    case '天津':
+      location = '2035836';
+      break;
+  }
+  $.ajax({
+    url: './json/' + location + '.json',
+    type: 'GET',
+    dataType: 'json'
+  }).done(function(data, textStatus, jqXHR) {
+    console.log('HTTP Request Succeeded: ' + jqXHR.status);
+    console.log(data);
+    if (data.records.length === 0) {
+      $('#noData').fadeIn('slow').animate({opacity: 1.0}, 2500).fadeOut('slow');
+    }
+    var mappedObjects = $.map(data.records, function(item) {
+      var imageThumb = item.primaryimageurl + '?width=600';
+      return new ArtObject(item.title, item.department, item.primaryimageurl, imageThumb, item.dated, item.period, item.culture, item.dimensions, item.creditline);
+    });
+    console.log(mappedObjects);
+    appViewModel.artObjects(mappedObjects);
+    addLightbox();
+  }).fail(function(jqXHR, textStatus, errorThrown) {
+    console.log('HTTP Request Failed');
   });
 }
 
